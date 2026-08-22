e1471ac8ac56779b3002488810d68a87183ce55e
sadegh694-glitch <sadegh694@gmail.com>
2026-08-22 21:40:23 +0330

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE_PREFIX = "ca-engineering-docs-";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((key) => key.startsWith(CACHE_PREFIX)).map((key) => caches.delete(key))))
+      .then(() => self.registration.unregister())
+  );
+});
